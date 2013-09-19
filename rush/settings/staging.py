183ae8f2a84ae9a76b4484164ac97791df6db567
Andrew <abeinstein@uchicago.edu>
2013-09-18 23:11:09 -0500

    Modified staging settings

--- rush/settings/staging.py
+++ rush/settings/staging.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['*']
 import os
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'static'),
