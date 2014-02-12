6193020a030ddd9b8b7c8c3a88044aff5e115d67
Andrew <abeinstein@uchicago.edu>
2014-02-12 15:35:31 +0100

    More tests

--- rush_app/tests/test_views.py
+++ rush_app/tests/test_views.py
@@ -13,27 +13,5 @@ class RushViewsTestCase(TestCase):
         login_success = self.client.login(username='slewis', password='password')
         self.assertTrue(login_success)
 
-    def test_signup_new_frat(self):
-        self.assertFalse(Frat.objects.filter(name="Psi Upsilon", university="Amherst College").exists())
-
-        data = {
-            'new_frat_created': True,
-            'first_name': 'John',
-            'last_name': 'Smith',
-            'email': 'jsmith@mail.com',
-            'username': 'jsmith',
-            'password': 'password',
-            'password_confirmation': 'password',
-            'frat': 'Psi Upsilon',
-            'school': 'Amherst College',
-            'frat_password': 'fratpassword',
-            'frat_password_confirmation': 'fratpassword'
-        }
-
-        resp = self.client.post('/signup', data)
-        self.assertEqual(resp.status_code, 301)
-
-        self.assertTrue(Frat.objects.filter(name="Psi Upsilon", university="Amherst College").exists())
-
 
 
